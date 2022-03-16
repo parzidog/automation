37eeffcd2dca16741f91b68a2ec28912340c6d8c
parzidog <83312149+parzidog@users.noreply.github.com>
2022-03-16 13:17:25 -0500

    Update CLIInstalls.py

--- CLIInstalls.py
+++ CLIInstalls.py
@@ -1,3 +1,3 @@
 import os
 
-os.system('pip install --upgrade pip; pip install npm; pip install selenium; pip install webdriver-manager')
+os.system('pip install --upgrade pip; pip install npm; pip install selenium; pip install webdriver-manager, pip install QtGui')
