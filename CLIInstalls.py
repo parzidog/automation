13be8acead3db6d0c587c3136ae577d542521fb0
parzidog <83312149+parzidog@users.noreply.github.com>
2022-03-16 09:14:43 -0500

    Update GUI and OS files

--- CLIInstalls.py
+++ CLIInstalls.py
@@ -1,3 +1,3 @@
 import os
 
-os.system('pip install --upgrade pip; pip install npm; pip install selenium; pip install webdriver-manager')
+os.system('pip install --upgrade pip; pip install npm; pip install selenium; pip install webdriver-manager; python -m pip install python-lsp-server[all]; python -m pip install git+https://github.com/tomv564/pyls-mypy.git; pip3 install PySide2')
--- app.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python /downloads/wfReports.py
