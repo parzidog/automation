58419e3b97f363731a522f0f375129f6602df2c9
parzidog <83312149+parzidog@users.noreply.github.com>
2022-03-18 13:08:20 -0500

    Update CLIInstalls.py

--- CLIInstalls.py
+++ CLIInstalls.py
@@ -1,3 +1,3 @@
 import os
 
-os.system('pip install --upgrade pip; pip install npm; pip install selenium; pip install webdriver-manager; pip install QtGui; pip install pyqt5-tools; pip install PyQtWebEngine')
+os.system('pip install --upgrade pip;; pip install PyQt5; pip install npm; pip install selenium; pip install webdriver-manager; pip install PyQt5-Tools; pip install QtGui; pip install pyqt5-tools; pip install PyQtWebEngine; pip install python-lsp-server[all]; pip install git+https://github.com/tomv564/pyls-mypy.git')
