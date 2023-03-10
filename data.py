dc1384343b45b9da87d18159e592a1ba674d5323
Kenny Schaefer <kenneth.schaefer0@gmail.com>
2023-03-10 15:26:49 -0600

    Extract data to seperate file. Need to update main files to condense

---
+++ data.py
@@ -0,0 +1,90 @@
+facilities = {
+  'amarillo': {
+    username: 'swirlcake_planes',
+    password: 'Storage1!',  
+  },
+  'austin': {
+    username: 'swirlcake_aus',
+    password: 'Storage!1',
+  },
+  'caddo_mills': {
+    username: 'swirlcake_45',
+    password: 'Storage5!',
+  },
+  'dallas': {
+    username: 'swirlcake_45',
+    password: 'Storage5!',
+  },
+  'enid': {
+    username: 'swirlcake_45',
+    password: 'Storage5!',
+  },
+  ennis: {
+    username: 'swirlcake_45',
+    password: 'Storage5!',
+  },
+  'gresham1': {
+    username: 'swirlcake_45',
+    password: 'Storage5!',
+  },
+  'gresham2': {
+    username: 'swirlcake_45',
+    password: 'Storage5!',
+  },
+  'hobbs': {
+    username: 'swirlcake_45',
+    password: 'Storage5!',
+  },
+  'kaufman': {
+    username: 'tocarro_lions',
+    password: 'Storage1!',
+  },
+  'kaufman2': {
+    username: 'tocarro_lions',
+    password: 'Storage1!',
+  },
+  'krum': {
+    username: 'swirlcake_45',
+    password: 'Storage5!',
+  },
+  'laughlin': {
+    username: 'swirlcake_nv',
+    password: '0903@Jerry!',
+  },
+  'lindale': {
+    userneame: 'swirlcake_45',
+    password: 'Storage5!',
+  },
+  'lubbock': {
+    username: 'swirlcake_45',
+    password: 'Storage5!',
+  },
+  'mineola': {
+    username: 'swirlcake_45',
+    password: 'Storage5!',
+  },
+  'mineola_2': {
+    username: 'swirlcake_45',
+    password: 'Storage5!',
+  },
+  'paris': {
+    username: 'swirlcake_45',
+    password: 'Storage5!',
+  },
+  'seminole': {
+    username: 'swirlcake_45',
+    password: 'Storage5!',
+  },
+  'stillwater': {
+    username: 'swirlcake_45',
+    password: 'Storage5!',
+  },
+  'tulsa': {
+    username: 'swirlcake_45',
+    password: 'Storage5!',
+  },
+  'wichita_falls': {
+    username: 'swirlcake_wf',
+    password: 'Storage1!',
+  },
+}
